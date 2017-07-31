7afe99452ebd6377ba6c9dc34e915942d57fdea2
Pickles726 <danielflindsay@gmail.com>
2017-07-31 23:53:48 +0000

    Working

--- cameratmp01.py
+++ cameratmp01.py
@@ -2,16 +2,18 @@ from datetime import datetime
 from picamera import PiCamera
 import time
 
+#DATE = datetime.now().strftime('%m-%d-%y_%H%M%S')
 # Returns current time
 def getTime():
 	return datetime.now().hour*3600 + datetime.now().minute*60 + datetime.now().second
 
 def takePicture():
 #	PiCamera().start_preview()
-	time.sleep(2)
+#	time.sleep(2)
 #	PiCamera().capture('img{timestamp:%Y-%m-%d-%H-%M}.jpg')
-	with open PiCamera() as camera:
-	camera.capture('img{timestamp:%Y-%m-%d-%H-%M}.jpg')
+	with PiCamera() as camera:
+		DATE = datetime.now().strftime('%m-%d-%y_%H%M%S')
+		camera.capture('img%s.jpg' % DATE)
 
 def main():
 	takePicture()
