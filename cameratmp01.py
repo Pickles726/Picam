f571b937de2f72f21e9487e02ac505882ae13db8
Shane <snielson@users.noreply.github.com>
2017-07-31 13:18:31 -0600

    Update cameratmp01.py
    Formatting

--- cameratmp01.py
+++ cameratmp01.py
@@ -2,18 +2,22 @@ from datetime import datetime
 from picamera import PiCamera
 import time
 
+# Returns current time
 def getTime():
 	return datetime.now().hour*3600 + datetime.now().minute*60 + datetime.now().second
+
 def takePicture():
 	PiCamera.start_preview()
-	sleep(2)
+	time.sleep(2)
 	camera.capture_continuous('img{timestamp:%Y-%m-%d-%H-%M}.jpg')
+
 def main():
-	getTime()
 	takePicture()
 	time.sleep(3600)
+
+# Runs main() if getTime() meets the time range
 while True:
-if getTime() <=25200 or getTime() >= 64800
-	main()
+	if getTime() <= 25200 or getTime() >= 64800
+		main()
 
 sys.exit(0)
