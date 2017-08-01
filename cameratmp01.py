356c35411455468099110cf3c3618310b899b901
Pickles726 <danielflindsay@gmail.com>
2017-08-01 14:16:17 -0600

    TweakingPiCam

--- cameratmp01.py
+++ cameratmp01.py
@@ -14,6 +14,8 @@ def takePicture():
 #	time.sleep(2)
 #	PiCamera().capture('img{timestamp:%Y-%m-%d-%H-%M}.jpg')
 	with PiCamera() as camera:
+		time.sleep(2)
+		camera.resolution = (3280, 2464)
 		DATE = datetime.now().strftime('%m-%d-%y_%H%M%S')
 		camera.capture('/media/pi/pondsusb/img%s.jpg' % DATE)
 
@@ -25,6 +27,6 @@ while True:
 	print getTime()
 	if getTime() >=25200 or getTime() <= 64800:
 		takePicture()
-	time.sleep(10)
+	time.sleep(3598)
 
 sys.exit(0)
